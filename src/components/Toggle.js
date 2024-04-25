90854c2ca06af2a90df82a7f3b23147663d9cb60
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-04-25 16:00:07 +0300

    state and events code-along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,35 @@
-import React from "react";
+/*import React from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  //handleButton function
+  
+  function handleButton(event) {
+    event.target.textContent = event.target.textContent === 'ON' ? 'OFF' : 'ON';
+  }
+  return (
+        <>
+          <button onClick={handleButton}>OFF</button>
+        </>
+  )
 }
 
 export default Toggle;
+*/
+import React,{ useState} from "react";
+
+
+ export default function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+
+  //handleButton function
+  // assign arrow function to a variable
+  const toggleButton =(event) => {
+    setIsOn(!isOn)
+  };
+  return (
+    <button onClick={toggleButton}>
+      {isOn ? 'ON' : 'OFF'}
+    </button>
+  )
+}
+
